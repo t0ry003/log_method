13fb803798c9fd18c2d47bc1c9236c596c978ed0
Olteanu Rares <rares.olteanu@cnodobescu.ro>
2021-05-03 12:23:36 +0300

    Fixed bad notations

--- main.cpp
+++ main.cpp
@@ -11,22 +11,25 @@ using namespace std;
 string log_FolderName = "log";
 
 // Log Folder initialisation
-void log_folder(){
+void log_folder() {
     system(("mkdir " + log_FolderName).c_str());
-    cout << "folder: " << '"' << log_FolderName << '"' << " Created!";
+    cout << "folder: " << '"' << log_FolderName << '"' << " Created!" << endl;
 }
 
 // Error push to log file
-void error_Log(int er_code, string er_text){
+void error_Log(int er_code, string er_text) {
 //    error_Log method accepts only an int and a string text exception!
-        ofstream log("logs/error.log");
-        log << er_code << " " << er_text << '\n';
-        cout << "Exception: Check " << '"' << log_FolderName << '"';
+    string log_FileName = "error.log";
+    string log_FilePath = log_FolderName + "/" + log_FileName;
+    ofstream log((log_FolderName + "/" + log_FileName).c_str());
+    log << er_code << " " << er_text << '\n';
+    cout << endl << "Exception '" << er_code << "' : Check " << '"' << log_FilePath << '"';
 }
 
 int main() {
 //    init the log folder by "log_folder()"
     log_folder();
 //    example of error push to log file
-    error_Log(420, "!Error Text!");
+    error_Log(420, "Error Text");
+    error_Log(404, "Not found");
 }
