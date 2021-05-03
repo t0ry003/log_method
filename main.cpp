4fefd0c68177c5efcaf369f0667710de0b825765
Olteanu Rares <rares.olteanu@cnodobescu.ro>
2021-05-03 12:01:29 +0300

    Easier to understand

--- main.cpp
+++ main.cpp
@@ -7,23 +7,26 @@
 
 using namespace std;
 
-ofstream results("results.txt");
+// Global foldername for easy use
+string log_FolderName = "log";
 
-string log_FolderName;
-
+// Log Folder initialisation
 void log_folder(){
-    log_FolderName = "logs";
     system(("mkdir " + log_FolderName).c_str());
-    cout << '"' << log_FolderName << '"' << " Created!";
+    cout << "folder: " << '"' << log_FolderName << '"' << " Created!";
 }
 
+// Error push to log file
 void error_Log(int er_code, string er_text){
 //    error_Log method accepts only an int and a string text exception!
         ofstream log("logs/error.log");
         log << er_code << " " << er_text << '\n';
+        cout << "Exception: Check " << '"' << log_FolderName << '"';
 }
 
 int main() {
+//    init the log folder by "log_folder()"
     log_folder();
+//    example of error push to log file
     error_Log(420, "!Error Text!");
 }
